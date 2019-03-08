281454d852e57952ea4bcc90697cfda93796a575
Tony Li <litony1999@gmail.com>
2019-03-08 18:10:44 -0500

    validation?????

--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validate_even(value):
+    if len(value) > 10:
+        raise ValidationError(
+            print('error')
+        )
+
 
 
 class Product(models.Model):
 	name = models.CharField(max_length=255)
-	description = models.CharField(max_length=255)
+	description = models.TextField(validators=[validate_even])
 	price = models.CharField(max_length=255)
 
 
